58ca872d2c8931c6125700147e878b7eb314edb2
MariaRuiz-96 <marijo.ruch1996@gmail.com>
2024-10-11 17:48:25 -0600

    Crear funcion getComputerChoice()
    se creo esta funcion para que de manera random se escoja una de las 3 opciones de piedra, papel o tijera, para darle como opcion que escoje la computadora

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
 'use strict'
+function getComputerChoice(){
+    let num = Math.floor(Math.random()*3)
+
+    console.log(num)
+    
+    if(num == 0){
+        return "piedra";
+    } else if (num == 1){
+        return "papel";
+    } else {
+        return "tijera";
+    }
+    
+}
+console.log(getComputerChoice())
+
+function getHumanChoice(){
+    let jugador
+
+    do{
+        jugador = prompt("Pidra, Papel o Tijera?");
+        jugador = jugador.toLowerCase()
+    } while( jugador !== "piedra" && jugador !== "papel" && jugador !== "tijera");
+
+    return jugador
+}
+
+getHumanChoice()
+
+
+
